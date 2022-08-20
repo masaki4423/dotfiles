c617369b82c6aea8f38955bd2d2361962b74b471
masaki4423 <kashiwabara356@gmail.com>
2022-08-20 22:34:51 +0900

    add macOS setting

--- .wezterm.lua
+++ .wezterm.lua
@@ -27,4 +27,24 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
             {key = 'x', mods = 'CTRL|ALT', action = wezterm.action.ActivateCopyMode},
         },
     }
+elseif wezterm.target_triple == 'aarch64-apple-darwin' then
+    return {
+        font = font,
+        color_scheme = color_scheme,
+        window_background_opacity = window_background_opacity,
+        hide_tab_bar_if_only_one_tab = hide_tab_bar_if_only_one_tab,
+        keys = {
+            {key = 'b', mods = 'CTRL|CMD', action = wezterm.action.ActivatePaneDirection 'Left'},
+            {key = 'f', mods = 'CTRL|CMD', action = wezterm.action.ActivatePaneDirection 'Right'},
+            {key = 'p', mods = 'CTRL|CMD', action = wezterm.action.ActivatePaneDirection 'Up'},
+            {key = 'n', mods = 'CTRL|CMD', action = wezterm.action.ActivatePaneDirection 'Down'},
+            {key = 'LeftArrow', mods = 'CTRL|CMD', action = wezterm.action.AdjustPaneSize {'Left', 1}},
+            {key = 'RightArrow', mods = 'CTRL|CMD', action = wezterm.action.AdjustPaneSize {'Right', 1}},
+            {key = 'UpArrow', mods = 'CTRL|CMD', action = wezterm.action.AdjustPaneSize {'Up', 1}},
+            {key = 'DownArrow', mods = 'CTRL|CMD', action = wezterm.action.AdjustPaneSize {'Down', 1}},
+            {key = 'RightArrow', mods = 'CMD', action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain'}},
+            {key = 'DownArrow', mods = 'CMD', action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'}},
+            {key = 'x', mods = 'CTRL|CMD', action = wezterm.action.ActivateCopyMode},
+        }
+    }
 end
