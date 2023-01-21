f79613369e376e372dc75e888ab152c4b067af2b
masaki4423 <kashiwabara356@gmail.com>
2023-01-21 19:41:09 +0900

    change neo-tree setting and add colorscheme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,6 +2,20 @@ vim.cmd.packadd "packer.nvim"
 
 require("packer").startup(function()
     use 'wbthomason/packer.nvim'
+    -- ColorScheme
+    use {
+        'shaunsingh/nord.nvim',
+        config = function()
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = true
+            vim.g.nord_disable_background = true
+            vim.g.nord_italic = true
+            vim.g.nord_uniform_diff_background = false
+            vim.g.nord_bold = true
+            require('nord').set()
+            vim.cmd[[colorscheme nord]]
+        end
+    }
 
     use {
         'nvim-lualine/lualine.nvim',
@@ -41,9 +55,25 @@ require("packer").startup(function()
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", 
             "MunifTanjim/nui.nvim",
-        }
+        },
+        config = function()
+            require("neo-tree").setup({
+                close_if_last_window = true,
+                filesystem = {
+                    filtered_items = {
+                        visible = false,
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
+            })
+            vim.api.nvim_create_autocmd("VimEnter", {
+                command = "Neotree toggle"
+            })
+        end
     }
 end)
+
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "plugins.lua" },
