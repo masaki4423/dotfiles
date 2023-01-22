ffc36b02739d950f87fdff846f80a26208e989a4
masaki4423 <kashiwabara356@gmail.com>
2023-01-22 19:13:08 +0900

    change autocmd

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -99,7 +99,9 @@ require("packer").startup(function()
     }
 end)
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "plugins.lua" },
-  command = "PackerCompile",
-})
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
