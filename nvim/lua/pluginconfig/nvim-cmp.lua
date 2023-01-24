9466e42dcd7abdaf9454dff01031ea4b29280555
masaki4423 <kashiwabara356@gmail.com>
2023-01-24 20:21:36 +0900

    change document highlight setting

--- nvim/lua/pluginconfig/nvim-cmp.lua
+++ nvim/lua/pluginconfig/nvim-cmp.lua
@@ -69,22 +69,3 @@ cmp.setup {
    end
   }
 }
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    -- capabilities = capabilities
--- }
-
-vim.cmd [[
-set updatetime=500
-highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-augroup lsp_document_highlight
-    autocmd!
-    autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-    autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-    augroup END
-]]
