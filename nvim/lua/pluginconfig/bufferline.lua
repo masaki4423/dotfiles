99c117c997c3c03daa0142c7432650a23bf5f42d
masaki4423 <kashiwabara356@gmail.com>
2023-01-26 11:32:28 +0900

    change bufferline setting

--- nvim/lua/pluginconfig/bufferline.lua
+++ nvim/lua/pluginconfig/bufferline.lua
@@ -1,1 +1,29 @@
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+        numbers = function(opts)
+            return string.format('%s·%s', opts.raise(opts.ordinal), opts.lower(opts.id))
+        end,
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                    or (e == "warning" and " " or "" )
+            s = s .. n .. sym
+            end
+            return s
+        end,
+        offsets = {
+            {
+                filetype = "neo-tree",
+                text = "File Explorer",
+                highlight = "Directory",
+                separator = true
+            }
+        }
+    },
+}
+
+vim.keymap.set("n", "<leader>r", "<Cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>t", "<Cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", "<Cmd>BufferLinePickClose<CR>", { noremap = true, silent = true })
