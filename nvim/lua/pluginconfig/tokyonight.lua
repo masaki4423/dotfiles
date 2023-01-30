9e44e5181dda4fd5bb71eda34262466e8f5c9381
masaki4423 <kashiwabara356@gmail.com>
2023-01-30 16:19:29 +0900

    chnage tokyonight setting

--- nvim/lua/pluginconfig/tokyonight.lua
+++ nvim/lua/pluginconfig/tokyonight.lua
@@ -5,9 +5,21 @@ require("tokyonight").setup({
         comments = { bold = true },
         sidebars = "transparent",
     },
+    on_colors = function(colors)
+        colors.bg_highlight = "#636da6"
+    end,
     on_highlights = function(hl, c)
+        hl.LspReferenceRead = {
+            bg = "#636da6",
+        }
+        hl.LspReferenceText = {
+            bg = "#636da6",
+        }
+        hl.LspReferenceWrite = {
+            bg = "#636da6",
+        }
         hl.LineNr = {
-            fg = '#636da6',
+            fg = "#636da6",
         }
     end
 })
