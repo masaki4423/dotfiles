20731063fed1f47dc2101ba69c6f57ff3963fa43
masaki4423 <kashiwabara356@gmail.com>
2023-04-24 13:23:56 +0900

    change terminal setting

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -1,6 +1,11 @@
-vim.api.nvim_create_autocmd( {"TermOpen"}, {
-    pattern = "*",
-    command = "set nonumber | startinsert",
+-- Windowsを使用している場合はデフォルトのターミナルをPowerShellに設定
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	vim.opt.shell = "pwsh"
+end
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	pattern = "*",
+	command = "set nonumber | startinsert",
 })
 -- ターミナルモード時はESCを押下でインサートモードからターミナルノーマルモードに移行させるキーマップ
-vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", {silent=true})
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { silent = true })
