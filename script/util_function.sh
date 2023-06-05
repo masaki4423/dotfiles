dedc08f6cf72f04979d4008aadeedb97e9cb8d85
masaki4423 <kashiwabara356@gmail.com>
2023-06-05 14:56:38 +0900

    change link creation method

--- script/util_function.sh
+++ script/util_function.sh
@@ -16,25 +16,8 @@ function detect_os() {
 
 function create_symbolic_link() {
     mkdir -p ${HOME}/.config
-    if [ -L ${HOME}/.zshrc ]; then
-        unlink ${HOME}/.zshrc
-    elif [ -f ${HOME}/.zshrc ]; then
-        rm ${HOME}/.zshrc
-    fi
 
-    if [ -L ${HOME}/.config/nvim ]; then
-        unlink ${HOME}/.config/nvim
-    elif [ -f ${HOME}/.config/nvim ]; then
-        rm -r ${HOME}/.config/nvim
-    fi
-
-    if [ -L ${HOME}/.wezterm.lua ]; then
-        unlink ${HOME}/.wezterm.lua
-    elif [ -f ${HOME}/.wezterm.lua ]; then
-        rm ${HOME}/.wezterm.lua
-    fi
-
-    ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
-    ln -s ${HOME}/.dotfiles/nvim ${HOME}/.config/nvim
-    ln -s ${HOME}/.dotfiles/.wezterm.lua ${HOME}/.wezterm.lua
+    ln -sf ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+    ln -snf ${HOME}/.dotfiles/nvim ${HOME}/.config/nvim
+    ln -sf ${HOME}/.dotfiles/.wezterm.lua ${HOME}/.wezterm.lua
 }
