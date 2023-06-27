59676eb2909452d81426d102dd7da7365508d6b0
masaki4423 <kashiwabara356@gmail.com>
2023-06-28 01:11:48 +0900

    update: update hammerspoon setting

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,13 @@
 hs.hotkey.bind({"cmd", "ctrl"}, "i", function()
     local appName = "WezTerm"
     local app = hs.application.get(appName)
+    local window = app:allWindows()[1]
+    local mainScreen = hs.screen.mainScreen()
 
-    if app == nil or app:isHidden() or not(app:isFrontmost()) then
+    if app == nil or app:isHidden() then
+        window:moveToScreen(mainScreen)
+        hs.application.launchOrFocus(appName)
+    elseif not(app:isFrontmost()) then
         hs.application.launchOrFocus(appName)
     else
         app:hide()
