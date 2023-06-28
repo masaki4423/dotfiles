e3a126b2fa5c5bba3de98171a70edaa382eba38d
masaki4423 <kashiwabara356@gmail.com>
2023-06-29 04:16:00 +0900

    update: update hammerspoon setting

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,32 @@
-hs.hotkey.bind({"cmd", "ctrl"}, "i", function()
+local application = require("hs.application")
+local screen = require("hs.screen")
+
+local double_press = require("ctrlDoublePress")
+
+local screen_watcher = screen.watcher.newWithActiveScreen(function()
     local appName = "WezTerm"
-    local app = hs.application.get(appName)
+    local app = application.get(appName)
     local window = app:allWindows()[1]
-    local mainScreen = hs.screen.mainScreen()
+    local mainScreen = screen.mainScreen()
 
     if app == nil or app:isHidden() then
         window:moveToScreen(mainScreen)
-        hs.application.launchOrFocus(appName)
-    elseif not(app:isFrontmost()) then
-        hs.application.launchOrFocus(appName)
+    end
+end):start()
+
+local open_wezterm = function()
+    local appName = "WezTerm"
+    local app = application.get(appName)
+
+    if app == nil or app:isHidden() or not(app:isFrontmost()) then
+        application.launchOrFocus(appName)
     else
         app:hide()
     end
-end)
+
+    screen_watcher:start()
+
+end
+
+double_press.timeframe = 0.5
+double_press.action = open_wezterm
