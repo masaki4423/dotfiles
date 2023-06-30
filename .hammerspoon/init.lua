66ab3dd01429e9677ca9b9b1a04fe9e40e8adbcb
masaki4423 <kashiwabara356@gmail.com>
2023-07-01 05:15:42 +0900

    update: update hammerspoon setting

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,17 +1,52 @@
 local application = require("hs.application")
+local geometry = require("hs.geometry")
+local hotkey = require("hs.hotkey")
+local mouse = require("hs.mouse")
 local screen = require("hs.screen")
+local window = require("hs.window")
 
 local double_press = require("ctrlDoublePress")
+
+local window_filter = window.filter.new()
+
+local function focusedWindowFunc(focusedWindow)
+    local appName = "WezTerm"
+    local app = application.get(appName)
+    local appWindow = app:allWindows()[1]
+    local focusedWindowScreen = focusedWindow:screen()
+
+    if app == nil or app:isHidden() then
+        appWindow:moveToScreen(focusedWindowScreen)
+    end
+end
+
+local eventTable = {
+    windowFocused = focusedWindowFunc
+}
+
+window_filter:subscribe(eventTable)
+
+hotkey.bind({"cmd", "ctrl"}, "i", function()
+    local focusedWindow = window.focusedWindow()
+    local rect = focusedWindow:frame()
+    local center = geometry.rectMidPoint(rect)
+
+    mouse.absolutePosition(center)
+
+end)
+
 
 local screen_watcher = screen.watcher.newWithActiveScreen(function()
     local appName = "WezTerm"
     local app = application.get(appName)
-    local window = app:allWindows()[1]
-    local mainScreen = screen.mainScreen()
+    local appWindow = app:allWindows()[1]
+    local focusedWindow = window.focusedWindow()
+    local focusedWindowScreen = focusedWindow:screen()
 
     if app == nil or app:isHidden() then
-        window:moveToScreen(mainScreen)
+        appWindow:moveToScreen(focusedWindowScreen)
     end
+
 end):start()
 
 local open_wezterm = function()
