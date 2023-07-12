96dce59b175c12fd9dd4d67dc6f82ad4314141a3
masaki4423 <kashiwabara356@gmail.com>
2023-07-13 00:23:45 +0900

    change: change toggleterm setting

--- nvim/lua/pluginconfig/toggleterm.lua
+++ nvim/lua/pluginconfig/toggleterm.lua
@@ -12,5 +12,7 @@ require("toggleterm").setup({
     float_opts = {
         border = "single",
         winblend = 3,
+        width = math.floor(vim.o.columns * 0.9),
+        height = math.floor(vim.o.lines * 0.9),
     },
 })
