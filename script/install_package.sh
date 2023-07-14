b7e37460255848f59e19f6150ed2f6447d980f43
masaki4423 <kashiwabara356@gmail.com>
2023-07-15 06:09:49 +0900

    change: modified wezterm installation script

--- script/install_package.sh
+++ script/install_package.sh
@@ -137,14 +137,13 @@ function install_wezterm() {
     print_info "Install WezTerm"
 
     local tag_name
+    local distributor_id
+    local release_version
     tag_name=$(curl -s https://api.github.com/repos/wez/wezterm/releases/latest | jq -r '.tag_name')
+    distributor_id=$(lsb_release -is 2>/dev/null || sh -c "source /etc/os-release && echo \$NAME")
+    release_version=$(lsb_release -rs 2>/dev/null || sh -c "source /etc/os-release && echo \$VERSION_ID")
 
     if [ $(arch) == "x86_64" ]; then
-        local distributor_id
-        local release_version
-        distributor_id=$(lsb_release -is)
-        release_version=$(lsb_release -rs)
-
         wget https://github.com/wez/wezterm/releases/download/${tag_name}/wezterm-${tag_name}.${distributor_id}${release_version}.deb -P ${HOME}/
         sudo dpkg -i ${HOME}/wezterm-${tag_name}.${distributor_id}${release_version}.deb 
         rm ${HOME}/wezterm-${tag_name}.${distributor_id}${release_version}.deb 
@@ -155,13 +154,86 @@ function install_wezterm() {
         ./get-deps
         set -eu
         ${HOME}/.cargo/bin/cargo build --all --release
-        bash ci/deploy.sh
-        sudo dpkg -i wezterm-20230408-112425-69ae8472*.deb
+        make_wezterm_package
+        sudo dpkg -i wezterm-${tag_name}.${distributor_id}${release_version}.deb
         cd -
         rm -rf ${HOME}/wezterm/
     fi
 
     print_success "Installation Successed"
+}
+
+function make_wezterm_package() {
+    # This source code is mostly copied from wezterm's ci/deploy.sh.
+
+    TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y%m%d-%H%M%S")}
+
+    distro=$(lsb_release -is 2>/dev/null || sh -c "source /etc/os-release && echo \$NAME")
+    distver=$(lsb_release -rs 2>/dev/null || sh -c "source /etc/os-release && echo \$VERSION_ID")
+
+    rm -rf pkg
+    mkdir -p pkg/debian/usr/bin pkg/debian/DEBIAN pkg/debian/usr/share/{applications,wezterm}
+    cat > pkg/debian/control <<EOF
+Package: wezterm
+Version: ${TAG_NAME#nightly-}
+Architecture: $(dpkg-architecture -q DEB_BUILD_ARCH_CPU)
+Maintainer: Wez Furlong <wez@wezfurlong.org>
+Section: utils
+Priority: optional
+Homepage: https://wezfurlong.org/wezterm/
+Description: Wez's Terminal Emulator.
+ wezterm is a terminal emulator with support for modern features
+ such as fonts with ligatures, hyperlinks, tabs and multiple
+ windows.
+Provides: x-terminal-emulator
+Source: https://wezfurlong.org/wezterm/
+EOF
+
+        cat > pkg/debian/postinst <<EOF
+#!/bin/sh
+set -e
+if [ "\$1" = "configure" ] ; then
+        update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/open-wezterm-here 20
+fi
+EOF
+
+        cat > pkg/debian/prerm <<EOF
+#!/bin/sh
+set -e
+if [ "\$1" = "remove" ]; then
+	update-alternatives --remove x-terminal-emulator /usr/bin/open-wezterm-here
+fi
+EOF
+
+    install -Dsm755 -t pkg/debian/usr/bin target/release/wezterm-mux-server
+    install -Dsm755 -t pkg/debian/usr/bin target/release/wezterm-gui
+    install -Dsm755 -t pkg/debian/usr/bin target/release/wezterm
+    install -Dm755 -t pkg/debian/usr/bin assets/open-wezterm-here
+    install -Dsm755 -t pkg/debian/usr/bin target/release/strip-ansi-escapes
+
+    deps=$(cd pkg && dpkg-shlibdeps -O -e debian/usr/bin/*)
+    mv pkg/debian/postinst pkg/debian/DEBIAN/postinst
+    chmod 0755 pkg/debian/DEBIAN/postinst
+    mv pkg/debian/prerm pkg/debian/DEBIAN/prerm
+    chmod 0755 pkg/debian/DEBIAN/prerm
+    mv pkg/debian/control pkg/debian/DEBIAN/control
+    sed -i '/^Source:/d' pkg/debian/DEBIAN/control  # The `Source:` field needs to be valid in a binary package
+    echo $deps | sed -e 's/shlibs:Depends=/Depends: /' >> pkg/debian/DEBIAN/control
+    cat pkg/debian/DEBIAN/control
+
+    install -Dm644 assets/icon/terminal.png pkg/debian/usr/share/icons/hicolor/128x128/apps/org.wezfurlong.wezterm.png
+    install -Dm644 assets/wezterm.desktop pkg/debian/usr/share/applications/org.wezfurlong.wezterm.desktop
+    install -Dm644 assets/wezterm.appdata.xml pkg/debian/usr/share/metainfo/org.wezfurlong.wezterm.appdata.xml
+    install -Dm644 assets/wezterm-nautilus.py pkg/debian/usr/share/nautilus-python/extensions/wezterm-nautilus.py
+    install -Dm644 assets/shell-completion/bash pkg/debian/usr/share/bash-completion/completions/wezterm
+    install -Dm644 assets/shell-completion/zsh pkg/debian/usr/share/zsh/functions/Completion/Unix/_wezterm
+    install -Dm644 assets/shell-integration/* -t pkg/debian/etc/profile.d
+    debname=wezterm-$TAG_NAME.$distro$distver
+    fakeroot dpkg-deb --build pkg/debian $debname.deb
+
+    mv pkg/debian pkg/wezterm
+    tar cJf $debname.tar.xz -C pkg wezterm
+    rm -rf pkg
 }
 
 function main() {
