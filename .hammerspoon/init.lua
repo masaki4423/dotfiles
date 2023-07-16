37f96cffe7ba0e9b16fda054ccd8151e09ab8ee0
masaki4423 <kashiwabara356@gmail.com>
2023-07-17 04:22:54 +0900

    change: change hammerspoon setting

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,29 +2,29 @@ local application = require("hs.application")
 local geometry = require("hs.geometry")
 local hotkey = require("hs.hotkey")
 local mouse = require("hs.mouse")
-local screen = require("hs.screen")
+-- local screen = require("hs.screen")
 local window = require("hs.window")
 
 local double_press = require("ctrlDoublePress")
 
-local window_filter = window.filter.new()
+-- local window_filter = window.filter.new()
 
-local function focusedWindowFunc(focusedWindow)
-    local appName = "WezTerm"
-    local app = application.get(appName)
-    local appWindow = app:allWindows()[1]
-    local focusedWindowScreen = focusedWindow:screen()
+-- local function focusedWindowFunc(focusedWindow)
+--     local appName = "WezTerm"
+--     local app = application.get(appName)
+--     local appWindow = app:allWindows()[1]
+--     local focusedWindowScreen = focusedWindow:screen()
+--
+--     if app == nil or app:isHidden() then
+--         appWindow:moveToScreen(focusedWindowScreen)
+--     end
+-- end
 
-    if app == nil or app:isHidden() then
-        appWindow:moveToScreen(focusedWindowScreen)
-    end
-end
+-- local eventTable = {
+--     windowFocused = focusedWindowFunc
+-- }
 
-local eventTable = {
-    windowFocused = focusedWindowFunc
-}
-
-window_filter:subscribe(eventTable)
+-- window_filter:subscribe(eventTable)
 
 hotkey.bind({"cmd", "ctrl"}, "i", function()
     local focusedWindow = window.focusedWindow()
@@ -36,18 +36,18 @@ hotkey.bind({"cmd", "ctrl"}, "i", function()
 end)
 
 
-local screen_watcher = screen.watcher.newWithActiveScreen(function()
-    local appName = "WezTerm"
-    local app = application.get(appName)
-    local appWindow = app:allWindows()[1]
-    local focusedWindow = window.focusedWindow()
-    local focusedWindowScreen = focusedWindow:screen()
-
-    if app == nil or app:isHidden() then
-        appWindow:moveToScreen(focusedWindowScreen)
-    end
-
-end):start()
+-- local screen_watcher = screen.watcher.newWithActiveScreen(function()
+--     local appName = "WezTerm"
+--     local app = application.get(appName)
+--     local appWindow = app:allWindows()[1]
+--     local focusedWindow = window.focusedWindow()
+--     local focusedWindowScreen = focusedWindow:screen()
+--
+--     if app == nil or app:isHidden() then
+--         appWindow:moveToScreen(focusedWindowScreen)
+--     end
+--
+-- end):start()
 
 local open_wezterm = function()
     local appName = "WezTerm"
